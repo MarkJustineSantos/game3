d8514e94dfa2da2934f34a2004eb053af0d3c50c
Mark Justine Santos <114572934+MarkJustineSantos@users.noreply.github.com>
2024-06-18 14:05:19 +0800

    first-commit

---
+++ js/words.js
@@ -0,0 +1,39 @@
+let words = 
+    [
+{
+    word: "Pepe",
+    hint: "Jose Rizal's petname in his family"
+},
+{
+    word: "Paciano",
+    hint: "Rizal second father"
+},
+{
+    word: "His Mother",
+    hint: "Rizal first teacher"
+},
+{
+    word: "Calamba",
+    hint: "Rizal birthplace"
+},
+{
+    word: "Usman",
+    hint: "Rizal's dog name"
+},
+{
+    word: "Merchant",
+    hint: "The surname Mercado when translated to english means"
+},
+{
+    word: "Spain",
+    hint: "Rizal is considered the national hero of the Philippines for his role in the Philippine Revolution against?"
+},
+{
+    word: "Universidad Central de Madrid",
+    hint: "Rizal earned a degree in medicine"
+},
+{
+    word: "Bagumbayan",
+    hint: "Rizal was executed on December 30, 1896, by a firing squad in"
+}
+]   
